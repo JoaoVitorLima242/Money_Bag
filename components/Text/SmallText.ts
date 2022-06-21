2c58f2996bd9567848a1458f8ad291420c734c5f
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-21 17:43:42 -0300

    a lot of changes

---
+++ components/Text/SmallText.ts
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react"
+import styled from "styled-components/native"
+
+// Colors
+import { colors } from "../colors"
+
+const StyledText = styled.Text`
+    font-size: 13px;
+    color: ${colors.gray};
+    text-align: left;
+`
+
+const SmallText: FunctionComponent = () => {
+
+
+    return <></>;
+}
+
+export default SmallText
